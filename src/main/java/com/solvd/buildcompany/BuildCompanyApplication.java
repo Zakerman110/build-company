e03d4f3fb7c4e746051ddeab66f6c4f0d098fd4e
Artur Hostiuk <artur1100011@gmail.com>
2022-05-09 14:35:28 +0300

    first commit

---
+++ src/main/java/com/solvd/buildcompany/BuildCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.solvd.buildcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildCompanyApplication.class, args);
+    }
+
+}
